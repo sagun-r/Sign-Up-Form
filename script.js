bbbbb9d27481e19af70498481198b600a4a1b254
AmazingBandit <11012938+sagun-r@users.noreply.github.com>
2024-01-23 10:45:59 -0800

    Added password pattern and password mismatch error message

---
+++ script.js
@@ -0,0 +1,18 @@
+const submitBtn = document.querySelector(".submitBtn");
+
+submitBtn.addEventListener("click", (e) => {
+  const pwd = document.getElementsByClassName("pwd")[0].value;
+  const confirmPwd = document.getElementsByClassName("confirmPwd")[0].value;
+  const hiddenPwd = document.getElementsByClassName("hiddenPwd")[0];
+
+  //checks if value in password input
+  if (pwd) {
+    //checks if password has value and if it matches confirm password
+    if (pwd === confirmPwd) {
+      hiddenPwd.style.display = "none";
+    } else {
+      e.preventDefault();
+      return (hiddenPwd.style.display = "block");
+    }
+  }
+});
