3ed57275f0a1d00b91ddbbab842b252ef3bf5fb8
AmazingBandit <11012938+sagun-r@users.noreply.github.com>
2024-01-23 16:51:56 -0800

    Added line to clear out password not match message

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ submitBtn.addEventListener("click", (e) => {
   const pwd = document.getElementsByClassName("pwd")[0].value;
   const confirmPwd = document.getElementsByClassName("confirmPwd")[0].value;
   const hiddenPwd = document.getElementsByClassName("hiddenPwd")[0];
+  hiddenPwd.style.display = "none";
 
   //checks if value in password input
   if (pwd) {
